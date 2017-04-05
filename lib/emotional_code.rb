5e636fa037e41e15448ce1fd92c99c66259433c9
wataru kikuchi <kikuchi@fms-l45.fujisan.local>
2017-04-05 15:11:24 +0900

    WIP add ballon

--- lib/emotional_code.rb
+++ lib/emotional_code.rb
@@ -2,5 +2,52 @@ require "emotional_code/version"
 require "emotional_code/face"
 
 module EmotionalCode
+  ::String.class_eval do
+    def 😁
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+    
+    def 😂 
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+
+    def 😃
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+
+    def 😄
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+
+    def 😠
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+
+    def 😡
+      puts generate_ballon
+      puts "\n\n"
+      puts __method__
+    end
+
+    private
+      def generate_ballon
+        words = self.gsub(/(.+?)\n/, '＞　\1　＜' + "\n")
+        saying = <<-"EOS"
+        ＿人人人人人人人人＿
+        #{words}
+        ￣Y^Y^Y^Y^Y^Y^Y￣
+        EOS
+      end
+  end
 end
 
--- lib/emotional_code/faces/human.rb
+++ lib/emotional_code/faces/human.rb
@@ -1,31 +1,31 @@
 module EmotionalCode
   module Faces
     module Human
-      ::String.class_eval do
-        def 😁
-          puts "I'm happy."
-        end
-        
-        def 😂 
-          puts "I'm sad."
-        end
-
-        def 😃
-          puts "Smily!"
-        end
-
-        def 😄
-          puts "I'm so happy!"
-        end
-
-        def 😠
-          puts "I'm angry."
-        end
-
-        def 😡
-          puts "I'm so angry."
-        end
-      end
+      # ::String.class_eval do
+      #   def 😁
+      #     puts "I'm happy."
+      #   end
+      #   
+      #   def 😂 
+      #     puts "I'm sad."
+      #   end
+      #
+      #   def 😃
+      #     puts "Smily!"
+      #   end
+      #
+      #   def 😄
+      #     puts "I'm so happy!"
+      #   end
+      #
+      #   def 😠
+      #     puts "I'm angry."
+      #   end
+      #
+      #   def 😡
+      #     puts "I'm so angry."
+      #   end
+      # end
     end
   end
 end
